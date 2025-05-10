84a6c5ceacae99738c11f21794aa253d25c7d414
Pol Comí <67558603+PolComiPorcar@users.noreply.github.com>
2025-05-10 14:54:18 +0200

    Dissolve amb botó

---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Shaders/Dissolve/Dissolve.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    [SerializeField] Material m_Crystal;
+    [SerializeField] Material m_Sphere;
+
+    [SerializeField] private float dissolveSpeed = 0.5f;
+
+    const float MIN_CRYSTAL = -1f;
+    const float MAX_CRYSTAL = 1.5f;
+    const float MIN_SPHERE = 0f;
+    const float MAX_SPHERE = 1.35f;
+
+    private float cutoffHeight = 0f;
+    private bool isDissolving = false;
+    private int dissolveDirection = 1;
+
+    private void Start()
+    {
+        ToggleDissolve();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ToggleDissolve();
+    }
+
+    private void Update()
+    {
+        if (isDissolving)
+        {
+            cutoffHeight += Time.deltaTime * dissolveSpeed * dissolveDirection;
+            cutoffHeight = Mathf.Clamp01(cutoffHeight);
+
+            m_Crystal.SetFloat("_Cutoff_Height", Mathf.Lerp(MIN_CRYSTAL, MAX_CRYSTAL, cutoffHeight));
+            m_Sphere.SetFloat("_Cutoff_Height", Mathf.Lerp(MIN_SPHERE, MAX_SPHERE, cutoffHeight));
+
+            if (cutoffHeight == 0f || cutoffHeight == 1f) isDissolving = false;
+        }
+    }
+
+    public void ToggleDissolve()
+    {
+        if (cutoffHeight == 0f) dissolveDirection = 1;
+        else if (cutoffHeight == 1f) dissolveDirection = -1;
+
+        isDissolving = true;        
+    }
+}
